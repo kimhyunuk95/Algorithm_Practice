e96e6eaab213dda867eccd6e44b5db7591317129
김현욱 <105341749+kimhyunuk95@users.noreply.github.com>
2022-12-02 12:45:27 +0900

    Update 02_3052.py

--- 2212/02_3052.py
+++ 2212/02_3052.py
@@ -3,6 +3,6 @@ for _ in range(10):
     n.append(int(input())) # 수 10개를 입력받아 배열에 저장
 arr = list()
 for i in range(10):
-    arr.append(n[i] %42) # 42의 나머지를 저장
+    arr.append(n[i] %42) # 42로 나눈 나머지를 저장
 arr = set(arr) # set을 통해 중복된 숫자를 제거해줌
 print(len(arr)) # set의 길이가 곧 중복되지 않은 숫자의 갯수
