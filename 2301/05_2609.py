698f6adc11e032cb303c0bb0c944d6f863f55de8
김현욱 <105341749+kimhyunuk95@users.noreply.github.com>
2023-01-05 17:11:50 +0900

    Create 05_2609.py

---
+++ 2301/05_2609.py
@@ -0,0 +1,14 @@
+a, b = map(int, input().split())
+
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+print(gcd(a, b))
+print(lcm(a, b))
+
+# 유클리드 호제법을 사용하여 코드를 작성한다.
